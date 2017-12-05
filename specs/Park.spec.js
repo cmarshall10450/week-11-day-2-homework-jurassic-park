693219641051a4b52c18da8476a99f899bfc3186
cmarshall10450 <cmarshall10450@gmail.com>
2017-12-05 18:27:02 +0000

    should be able to remove all dinosaurs of a certain type - passing

--- specs/Park.spec.js
+++ specs/Park.spec.js
@@ -9,15 +9,30 @@ describe('Park', () => {
     park = new Park()
   })
 
-  it('should start with an empty enclosure', function (done) {
+  it('should start with an empty enclosure', (done) => {
     assert.deepStrictEqual(park.enclosure, [])
     done()
   })
 
-  it('should be able to add a dinosaur', function (done) {
+  it('should be able to add a dinosaur', (done) => {
     const dinosaur = new Dinosaur('Velociraptor', 5)
     park.addDinosaur(dinosaur)
     assert.deepStrictEqual(park.enclosure, [dinosaur])
+    done()
+  })
+
+  it('should be able to remove all dinosaurs of a certain type', (done) => {
+    const dinosaur1 = new Dinosaur('Velociraptor', 5)
+    const dinosaur2 = new Dinosaur('Stegasaurus', 10)
+
+    park.addDinosaur(dinosaur1)
+    park.addDinosaur(dinosaur2)
+
+    assert.deepStrictEqual(park.enclosure, [dinosaur1, dinosaur2])
+
+    park.removeByType('Velociraptor')
+
+    assert.deepStrictEqual(park.enclosure, [dinosaur2])
     done()
   })
 })
--- src/Park.js
+++ src/Park.js
@@ -6,4 +6,13 @@ Park.prototype.addDinosaur = function (dinosaur) {
   this.enclosure.push(dinosaur)
 }
 
+Park.prototype.removeByType = function (type) {
+  for (let dinosaur of this.enclosure) {
+    if (dinosaur.type === type) {
+      const index = this.enclosure.indexOf(dinosaur)
+      this.enclosure.splice(index, 1)
+    }
+  }
+}
+
 module.exports = Park
