09ced0ff64832d1dbf88257a60b0f6ddbbd7026b
cmarshall10450 <cmarshall10450@gmail.com>
2017-12-05 18:15:49 +0000

    should be able to add a dinosaur - passing

--- specs/Park.spec.js
+++ specs/Park.spec.js
@@ -13,4 +13,11 @@ describe('Park', () => {
     assert.deepStrictEqual(park.enclosure, [])
     done()
   })
+
+  it('should be able to add a dinosaur', function (done) {
+    const dinosaur = new Dinosaur('Velociraptor', 5)
+    park.addDinosaur(dinosaur)
+    assert.deepStrictEqual(park.enclosure, [dinosaur])
+    done()
+  })
 })
